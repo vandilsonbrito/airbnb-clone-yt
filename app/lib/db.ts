196b35c28ea7d233097e60832d3d861ea71cc8a8
vandilsonbrito <vandilson25@outlook.com>
2024-09-07 19:17:12 -0300

    Chore: create db prisma config

---
+++ app/lib/db.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+// This is for check if the app is running locally set prisma to global prisma, if is running on production use prisma client
